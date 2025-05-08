69d48bc835005e6d8a22fec492846fcbeddf5e4c
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-05-08 17:53:38 +0800

    feat (landing  page): added a header and welcome messages

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>El Perro Y El Gato Adoption Center</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Welcome to El Perro Y El Gato Adoption Center 🐶🐱</h1>
+        <p>🐾Find your new furry friend today!🐾</p>
+    </header>
 </body>
 </html>
