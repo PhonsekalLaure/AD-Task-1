0bb9266fd4dd590990480ec004b014fd51460d60
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-05-08 20:34:58 +0800

    fix (pets page): correct title and stylesheet link in index.php

--- page/pets/index.php
+++ page/pets/index.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>El Perro Y El Gato Adoption Center</title>
+    <link rel="stylesheet" href="href="../>assets/css/styles.css"">
 </head>
 <body>
-    
+    <?php
+        $menu = ["Home" => "index.php", "Pets" => "", "About Us" => ""]
+    ?>
+    <header>
+        <h1>Welcome to El Perro Y El Gato Adoption Center 🐶🐱</h1>
+        <p>🐾Find your new furry friend today!🐾</p>
+    </header>
+    <nav>
+        <ul>
+            <?php
+                foreach ($menu as $title => $link){
+                    echo "<li><a href = '$link'>$title</a></li>";
+                }
+            ?>
+        </ul>
+    </nav>
 </body>
 </html>
